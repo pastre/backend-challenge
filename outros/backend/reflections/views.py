1279c8c477e76b33ad146c11eccb487eb242a909
pastre <pastre68@gmail.com>
2020-04-15 16:52:21 -0300

    Properly returns serialized response

--- outros/backend/reflections/views.py
+++ outros/backend/reflections/views.py
@@ -6,10 +6,9 @@ from  .models import *
 
 def reflections(request, name = None):
 	if name == None:
-
 		if request.method != 'GET': return HttpResponseForbidden('Wrong method, sorry')
 
 		serialized = serializers.serialize('json', Reflection.objects.all())
-		
 		return HttpResponse(serialized)
+		
 	return HttpResponse(f'<h1>NOME TOSCO {name}</h1>')
