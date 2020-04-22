e0e1c244324fcc7bda7f7897e6dd8f60cf9fc6ef
pastre <pastre68@gmail.com>
2020-04-22 13:46:00 -0300

    Properly added reflections as submodule

--- outros/backend/reflections/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-	path('reflections/<int:name>', views.reflections, name='reflections'),
-	path('reflections', views.reflections, name='reflections'),
-]
